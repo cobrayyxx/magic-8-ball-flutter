8e1de87d85b3634aeaf4196fee4349ce1f8bea69
cobrayyxx <muhammadhazim86@gmail.com>
2020-07-22 13:50:39 +0700

    making BallPage  StatelessWidget

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,19 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('Ask Me Anything'),
+      ),
+    );
+  }
+}
