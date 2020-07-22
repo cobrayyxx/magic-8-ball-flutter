d906f3af62fb555bdaca72514d44bd2b9028b28d
cobrayyxx <muhammadhazim86@gmail.com>
2020-07-22 14:02:22 +0700

    Making StatefulWidget and FlatButton

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,26 @@ class BallPage extends StatelessWidget {
         backgroundColor: Colors.blue[900],
         title: Text('Ask Me Anything'),
       ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        onPressed: () {
+          print('I got clicked');
+        },
+        child: Image.asset('images/ball1.png'),
+      ),
     );
   }
 }
